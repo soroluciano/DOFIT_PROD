d9c64c86ab80a11491ee4bbefe5c53d26d33ac55
soroluciano <soro.luc@gmail.com>
2015-06-24 00:00:59 -0300

    modificaciones perfil usuario 24_06_2015

--- DoFit/aplication/protected/views/perfilSocial/indexA.php
+++ DoFit/aplication/protected/views/perfilSocial/indexA.php
@@ -1,47 +1,42 @@
 
 <?php
-    $form=$this->beginWidget('CActiveForm', array(
-        'id'=>'post-form',
-        'enableAjaxValidation' => FALSE,
-        'htmlOptions' => array('enctype' => 'multipart/form-data'),
-    ));
-?>
-<?php
-
+//<img src='/DOFIT_FINAL/DoFit/aplication/images/pat2.jpg'><a href=''></a></img>
 echo "
     <h1 xmlns=\"http://www.w3.org/1999/html\">Pagina inicial Perfil Social</h1>
     <div id='content_perfil'>
-        <p>Completa tu perfil</p>
-        <div class='p_datos'>
-            <div><p><span class='size_2'>Luciano Soro</span>, <span class='size_1'>25</span></p></div>
-            <div><p><span class='size_2'>194</span><span class='size_1'> amigos</span></p></div>
-        </div>
 
 
-        <div class='' id='p_agregar_foto'>
-            <img src=''><a href=''></a></img>
-            <div class=''></div>
-        </div>
-        <div class='p_foto_1'>
-            <img src='/DOFIT_FINAL/DoFit/aplication/images/pat1.jpg'><a href=''></a></img>
-            <div class=''></div>
-        </div>
-        <div class='p_foto_2'>
-            <img src='/DOFIT_FINAL/DoFit/aplication/images/pat2.jpg'><a href=''></a></img>
-            <div class=''></div>
-        </div>
-        <div class='p_foto_3'>
-            <img src='/DOFIT_FINAL/DoFit/aplication/images/pat3.jpg'><a href=''></a></img>
-            <div class=''></div>
-        </div>
-        <div class='p_foto_4'>
-            <img src='/DOFIT_FINAL/DoFit/aplication/images/pat4.jpg'><a href=''></a></img>
-            <div class=''></div>
-        </div>
-    </div>";
+                <p>Completa tu perfil</p>
+                <div class='p_datos'>
+                    <div><p><span class='size_2'>Luciano Soro</span>, <span class='size_1'>25</span></p></div>
+                    <div><p><span class='size_2'>194</span><span class='size_1'> amigos</span></p></div>
+                </div>
+
+                <div class='' id='p_agregar_foto'>
+                    <img src=''><a href=''></a></img>
+                    <div class=''></div>
+
+                </div>
+                <div class='p_foto_1'>
+                    <img src='/DOFIT_FINAL/DoFit/aplication/images/pat1.jpg'><a href=''></a></img>
+                    <div class=''></div>
+                </div>
+                <div class='p_foto_2'>
+
+                    <div class=''><input type='button' value='Create a popup window!' onclick='MakePopup(event)'/></div>
+                </div>
+                <div class='p_foto_3'>
+                    <img src='/DOFIT_FINAL/DoFit/aplication/images/pat3.jpg'><a href=''></a></img>
+                    <div class=''></div>
+                </div>
+                <div class='p_foto_4'>
+                    <img src='/DOFIT_FINAL/DoFit/aplication/images/pat4.jpg'><a href=''></a></img>
+                    <div class=''></div>
+                </div>
+
+    </div>"
+
+
+;
 
 ?>
-<?php
-  
-?>
-<?php $this->endWidget(); ?>
