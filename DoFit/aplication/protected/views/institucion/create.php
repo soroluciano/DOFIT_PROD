e9bd6b024120e629f8d08a03916efaa0a0d41e4f
dcastellini <dcastellini89@gmail.com>
2015-07-04 10:24:13 -0300

    nose cuantos commits van

--- DoFit/aplication/protected/views/institucion/create.php
+++ DoFit/aplication/protected/views/institucion/create.php
@@ -4,32 +4,55 @@
 /* @var $form CActiveForm */
 ?>
 
-
-<div class="form">
-    <?php $form=$this->beginWidget('CActiveForm', array(
-        'id'=>'InstitucionForm',
-        // Please note: When you enable ajax validation, make sure the corresponding
-        // controller action is handling ajax validation correctly.
-        // There is a call to performAjaxValidation() commented in generated controller code.
-        // See class documentation of CActiveForm for details on this.
-        'enableAjaxValidation'=>true,
-        'enableClientValidation'=>false,
-        'clientOptions'=>array(
-            'validateOnSubmit'=>true,
-        ),
-    ));?>
-
-    <div class="row">
-        <?php echo $form->labelEx($model,'email',array('for'=>"exampleInputEmail1")); ?>
-        <?php echo $form->textField($model,'email',array('class'=>"form-control",'placeholder'=>"email",'id'=>"exampleInputEmail1")); ?>
-        <?php echo $form->error($model,'email'); ?>
+<div class="navbar-wrapper">
+    <div class="container">
+        <nav class="navbar navbar-inverse navbar-static-top">
+            <div class="container">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="#">DoFit!</a>
+                </div>
+                <div id="navbar" class="navbar-collapse collapse">
+                    <div class="navbar-form navbar-right">
+                        <ul class="nav navbar-nav">
+                            <li class="active"><a href="#">Home</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </nav>
     </div>
+</div>
 
-    <div class="row">
-        <?php echo $form->labelEx($model,'password'); ?>
-        <?php echo $form->passwordField($model,'password');?>
-        <?php echo $form->error($model,'password'); ?>
+<!-- Carousel
+================================================== -->
+<div id="myCarousel" class="carousel slide" data-ride="carousel">
+    <div class="carousel-inner" role="listbox">
+        <div class="item active">
+            <img class="first-slide" src="<?php echo Yii::app()->request->baseUrl; ?>/img/10.jpg" alt="First slide">
+        </div>
     </div>
+</div>
+
+<div class="container">
+    <form>
+        <?php $form=$this->beginWidget('CActiveForm', array('id'=>'InstitucionForm', 'enableAjaxValidation'=>true, 'enableClientValidation'=>false, 'clientOptions'=>array('validateOnSubmit'=>true,),));?>
+        <div class="col-md-8">
+            <div class="form-group">
+                <?php echo $form->labelEx($model,'email',array('for'=>"exampleInputEmail1")); ?>
+                <?php echo $form->textField($model,'email',array('class'=>"form-control",'placeholder'=>"Email",'id'=>"exampleInputEmail1")); ?>
+                <?php echo $form->error($model,'email'); ?>
+            </div>
+            <div class="form-group">
+                <?php echo $form->labelEx($model,'password'); ?>
+                <?php echo $form->passwordField($model,'password');?>
+                <?php echo $form->error($model,'password'); ?>
+            </div>
 
     <div class="row">
         <?php
