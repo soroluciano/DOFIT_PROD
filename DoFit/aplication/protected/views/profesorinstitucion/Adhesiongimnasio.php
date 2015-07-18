7fd6e2f362b5ce1cb1a5d75febfb7d548d4286d5
dcastellini <dcastellini89@gmail.com>
2015-07-18 00:50:47 -0300

    damian los 3 cabiados

--- DoFit/aplication/protected/views/profesorinstitucion/Adhesiongimnasio.php
+++ DoFit/aplication/protected/views/profesorinstitucion/Adhesiongimnasio.php
@@ -16,7 +16,7 @@
                         <span class="icon-bar"></span>
                         <span class="icon-bar"></span>
                     </button>
-                    <img class="navbar-brand" src="<?php echo Yii::app()->request->baseUrl; ?>/img/logo_blanco.png" alt="First slide">
+                    <img class="navbar-brand-img" src="<?php echo Yii::app()->request->baseUrl; ?>/img/logo_blanco.png" alt="First slide">
                 </div>
                 <div id="navbar" class="navbar-collapse collapse">
                     <div class="navbar-form navbar-right">
@@ -42,10 +42,10 @@
 
 <!-- Carousel
 ================================================== -->
-<div id="myCarousel" class="carousel slide" data-ride="carousel">
-    <div class="carousel-inner" role="listbox">
+<div id="myCarousel" class="carousel_min slide" data-ride="carousel">
+    <div class="carousel-inner_min" role="listbox">
         <div class="item active">
-            <img class="first-slide" src="<?php echo Yii::app()->request->baseUrl; ?>/img/15.jpg" alt="First slide">
+            <img class="first-slide_min" src="<?php echo Yii::app()->request->baseUrl; ?>/img/16.jpg" alt="First slide">
         </div>
     </div>
 </div>
