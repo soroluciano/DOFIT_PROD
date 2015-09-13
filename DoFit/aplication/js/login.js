f32033b496b2c81e78736d208e3fa9d2e456bcb8
dcastellini <dcastellini89@gmail.com>
2015-09-13 18:23:55 -0300

    damian arreglo login

--- DoFit/aplication/js/login.js
+++ DoFit/aplication/js/login.js
@@ -2,33 +2,46 @@
  * Created by Damián on 12/09/2015.
  */
 
-function login(){
+function login() {
     debugger;
     //var algo = baseurl;
     var email = $('#email').val();
     var password = $('#password').val();
-  //concateno los datos a enviar por ajax
-    var data = {'email':email,'password':password};
+    $('#erruser').html("");
+    $('#errpass').html("");
+    var cont = 0;
+    if (email == "" || email == null){
+        $('#erruser').html("Ingrese su email");
+        cont++;
+    }
+    if (password == "" || password == null){
+        $('#errpass').html("Ingrese su contraseña");
+        cont++;
+    }
+    if(cont == 0) {
+        //concateno los datos a enviar por ajax
+        var data = {'email': email, 'password': password};
+        $.ajax({
+            url: baseurl + '/site/login',
+            type: "POST",
+            data: data,
+            dataType: "html",
+            cache: false,
+            success: function (response) {
+                if (response == "error") {
+                    alert("Datos incorrectos");
+                }
+                else {
+                    window.location.replace(response);
+                }
 
-    $.ajax({
-        url: baseurl+'/site/login',
-        type: "POST",
-        data: data,
-        dataType: "html",
-        cache: false,
-        success:function(response){
-            if(response=="error"){
-                alert("Datos incorrectos");
+            },
+            error: function (e) {
+                console.log(e);
             }
-            else{
-                window.location.replace(response);
-            }
+        });
+    }
 
-        },
-        error: function(e){
-            console.log(e);
-        }
-    });
 
 
 }
