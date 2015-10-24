bfe4194c35ed71055eeae199f59e1c3660574c38
robertomontoto <monti.rober9@gmail.com>
2015-10-24 01:37:05 -0300

    Roberto agrega modals para recuperar contraseña

--- DoFit/aplication/protected/views/usuario/Recuperarpassword.php
+++ DoFit/aplication/protected/views/usuario/Recuperarpassword.php
@@ -1,74 +1,76 @@
-<div class="navbar-wrapper">
+<script type="text/javascript">
+
+    $( window ).load(function() {
+        info();
+    });
+</script>
+
+<header class="navbar navbar-static-top bs-docs-nav" id="top" role="banner">
     <div class="container">
-        <nav class="navbar navbar-inverse navbar-static-top">
-            <div class="container">
-                <div class="navbar-header">
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-                        <span class="sr-only">Toggle navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-                    <a href='../'> <img class="navbar-brand-img" src="<?php echo Yii::app()->request->baseUrl; ?>/img/logo_blanco.png" alt="First slide"></a>
-                </div>
-                <div id="navbar" class="navbar-collapse collapse">
-                    <div class="navbar-form navbar-right">
-                        <ul class="nav navbar-nav">
-                            <li class="active"><a>Hola!</a></li>
-                            <li class="dropdown">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Configuración <span class="caret"></span></a>
-                                <ul class="dropdown-menu">
-                                    <li><a href="#">Home</a></li>
-                                    <li><a href="#">Anotarme en actividades</a></li>
-                                    <li><a href="#">Ver mis actividades</a></li>
-                                    <li role="separator" class="divider"></li>
-                                    <li class="dropdown-header">Privacidad</li>
-                                    <li><a href="#">Configuración</a></li>
-                                    <li><a href="#"><?php echo CHtml::link('Salir', array('site/logout')); ?></a></li>
-                                </ul>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
+        <div class="navbar-header">
+            <button class="navbar-toggle collapsed" type="button" data-toggle="collapse" data-target="#bs-navbar" aria-controls="bs-navbar" aria-expanded="false">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a href="../"><img class="navbar-brand-img" src="<?php echo Yii::app()->request->baseUrl; ?>/img/logo_blanco.png" alt="First slide"></a>
+            <a href="../" class="navbar-brand"></a>
+        </div>
+        <nav id="bs-navbar" class="collapse navbar-collapse">
+            <ul class="nav navbar-nav">
+                <li>
+                    <a href="../getting-started/">Getting started</a>
+                </li>
+                <li>
+                    <a href="../css/">CSS</a>
+                </li>
+                <li>
+                    <a href="../components/">Components</a>
+                </li>
+                <li>
+                    <a href="../javascript/">JavaScript</a>
+                </li>
+                <li>
+                    <a href="../customize/">Customize</a>
+                </li>
+            </ul>
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="http://themes.getbootstrap.com" onclick="ga('send', 'event', 'Navbar', 'Community links', 'Themes');">Themes</a></li>
+                <li><a href="http://expo.getbootstrap.com" onclick="ga('send', 'event', 'Navbar', 'Community links', 'Expo');">Expo</a></li>
+                <li><a href="http://blog.getbootstrap.com" onclick="ga('send', 'event', 'Navbar', 'Community links', 'Blog');">Blog</a></li>
+            </ul>
         </nav>
     </div>
-</div>
+</header>
 
-<!-- Carousel
-================================================== -->
-
-<div id="myCarousel" class="carousel_min slide" data-ride="carousel">
-    <div class="carousel-inner_min" role="listbox">
-        <div class="item active">
-            <img class="first-slide_min" src="<?php echo Yii::app()->request->baseUrl; ?>/img/8.png" alt="First slide">
-        </div>
-    </div>
-</div>
-<div>
-
-    <div class="form">
-        <div class="container">
-            <!-- <p class="note">Campos con <span class="required">*</span> son requeridos.</p>  -->
-            <div class="row">
-                <div class="col-md-6">
-                    <h2 class="bs-docs-featurette-title">Recuperar contraseña</h2>
-                    <br>
-                    <div> Ingrese el mail de cual desea recuperar la contraseña</div>
-                    <br>
-                    <?php){
-                    ?>
-                    <div class="form-group">
-                        <?php echo CHtml::beginForm('Recuperarpassword2','post'); ?>
-                        <?php echo CHtml::activeTextField($usuario,'email',array('class'=>"form-control",'placeholder'=>"email",'id'=>"exampleInputEmail1")); ?>
+<div class="form">
+    <div class="container">
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <!-- <p class="note">Campos con <span class="required">*</span> son requeridos.</p>  -->
+        <div class="row">
+            <div class="col-md-6">
+                <h2 class="bs-docs-featurette-title">Recuperar contraseña</h2>
+                <br>
+                <div> Ingrese el mail de cual desea recuperar la contraseña</div>
+                <br>
+                <?php
+                ?>
+                <div class="form-group">
+                    <form action="">
+                        <input type="text" name="email" id="email" class="form-control" placeholder="email"></input>
                         <br/>
-                        <?php echo CHtml::Button('Enviar',array('type'=>'submit','class'=>'btn btn-primary','name'=>'Enviar','data-target'=>'#Usuario', 'data-toggle'=>'modal')); ?>
-                        <?php echo CHtml::endForm(); ?>
+                        <input type="button" value="Enviar" class="btn btn-primary" id="enviardatos" name="Enviar"></input>
+                    </form>
+                    <?php
 
-                        <?php
-                        if (isset($_POST['Enviar'])){
-                            if($encontro == 0) {
-                                echo "<div class='modal fade' id='Usuario' tabindex='-1' role='dialog' aria-labelledby='myModalLabel'>
+
+                    echo "<div class='modal fade'  id='usuarioerroneo' tabindex='-1' role='dialog' aria-labelledby='myModalLabel'>
 								    <div class='modal-dialog' role='document'>
 										<div class='modal-content'>
 											<div class='modal-header'>
@@ -83,11 +85,11 @@
 										</div>
 										</div>
 									</div>
+								</div>
 								</div>";
 
-                            }
-                            if($encontro == 1) {
-                                echo "<div class='modal fade' id='Usuario' tabindex='-1' role='dialog' aria-labelledby='myModalLabel'>
+
+                    echo "<div class='modal fade'  id='usuarioexitoso' tabindex='-1' role='dialog' aria-labelledby='myModalLabel'>
 								    <div class='modal-dialog' role='document'>
 										<div class='modal-content'>
 											<div class='modal-header'>
@@ -103,10 +105,34 @@
 										</div>
 									</div>
 								</div>";
-                            }
-                        }?>
-                    </div>
+
+                    ?>
                 </div>
             </div>
         </div>
-			
+    </div>
+
+    <script type="text/javascript">
+        $("#enviardatos").on("click",function(){
+            var email = $('#email').val();
+            var data = {'email':email};
+            $.ajax({
+                url :  baseurl + '/usuario/Recuperarpassword',
+                type: "POST",
+                dataType : "html",
+                data : data,
+                cache: false,
+                success: function (response) {
+                    if(response == "error"){
+                        $('#usuarioerroneo').modal('show');
+                    }
+                    if(response == "exitoso") {
+                        $('#usuarioexitoso').modal('show');
+                    }
+                } ,
+                error: function (e) {
+                    console.log(e);
+                }
+            });
+        })
+    </script>
