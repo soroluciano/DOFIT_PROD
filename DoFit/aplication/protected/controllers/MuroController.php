c5b080c475d3766afaa169604114df6ac3a65155
soroluciano <soro.luc@gmail.com>
2015-11-02 20:18:29 -0300

    change

--- DoFit/aplication/protected/controllers/MuroController.php
+++ DoFit/aplication/protected/controllers/MuroController.php
@@ -12,7 +12,21 @@
 
 class MuroController extends Controller
 {
-    public function actionIndex()
+    public function actionIndexAlumno()
+    {
+      	$Us = Usuario::model()->findByPk(Yii::app()->user->id);
+		$perfilSocial = PerfilSocial::model()->find('id_usuario=:id_usuario',array(':id_usuario'=>$Us->id_usuario));
+        //$dataProvider = new CArrayDataProvider($arrayData);
+        
+		//$list = Yii::app()->db->createCommand('select nombre,direccion,telfijo,CASE id_dia WHEN 1 THEN "Lunes" WHEN 2 THEN "Martes" WHEN 3 THEN "Miercoles" WHEN 4 THEN "Jueves" WHEN 5 THEN "Viernes" WHEN 6 THEN "Sábado" WHEN 7 THEN "Domingo" END as id_dia,lpad(hora,2,"0") as hora,lpad(minutos,2,"0") as minutos,actividad.id_actividad from ficha_institucion,actividad, actividad_horario where actividad.id_actividad not in (select id_actividad from actividad_alumno where id_usuario = '.Yii::app()->user->id.') and actividad.id_institucion = ficha_institucion.id_institucion and actividad.id_actividad = actividad_horario.id_actividad and ficha_institucion.id_institucion = (select id_institucion from ficha_institucion where id_localidad = ' . $_POST['Localidad']['id_localidad'] . ') and actividad.id_deporte = ' . $_POST['deporte'] . ' order by nombre')->queryAll();
+        $list = Yii::app()->db->createCommand('select id_posteo,posteo where id_actividad =' . $_POST['id_actividad']')->queryAll();
+       
+        $this->render('indexAlumno',array('usuario'=>$Us,'perfilSocial'=>$perfilSocial,'listPosts'=>$list));
+        
+  
+    }
+	
+	    public function actionIndexProfesor()
     {
       	$Us = Usuario::model()->findByPk(Yii::app()->user->id);
 		$perfilSocial = PerfilSocial::model()->find('id_usuario=:id_usuario',array(':id_usuario'=>$Us->id_usuario));
@@ -36,6 +50,7 @@ class MuroController extends Controller
                 'pages' => $pages,
             ));*/
     }
+	
 		
 		public function actionPrueba()
     {
@@ -76,5 +91,14 @@ class MuroController extends Controller
 				
 				
 		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
 }
 
--- DoFit/aplication/protected/views/muro/index.php
+++ DoFit/aplication/protected/views/muro/index.php
@@ -26,21 +26,6 @@ if(!Yii::app()->user->isGuest){
   
 	<div class="posts">
 		<div class="container">		
-
-				<div class="sidebar-nav">
-            <div class="well" style="width:300px; padding: 8px 0;">
-        		<ul class="nav nav-list"> 
-        		  <li class="nav-header">Main</li>        
-        		  <li class="active"><a href="index"><i class="icon-home"></i> Dashboard</a></li>
-        		  <li><a href="#"><i class="icon-edit"></i> Add Blog Post</a></li>
-                  <li><a href="#"><i class="icon-calendar"></i> Calendar</a></li>
-        		  <li><a href="#"><i class="icon-user"></i> Members</a></li>
-        		  <li><a href="#"><i class="icon-comment"></i> Comments</a></li>
-        		  <li><a href="#"><i class="icon-picture"></i> Gallery</a></li>
-        		</ul>
-        	</div>
-        </div>
-		
 		
 			<div class="row">
 			<!--	<div class="col-lg-3 col-sm-6">
