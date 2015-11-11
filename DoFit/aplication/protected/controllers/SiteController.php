bfb0112a5fb1bc9660bee662fc24e478199cf5da
soroluciano <soro.luc@gmail.com>
2015-11-11 11:53:55 -0300

    bug index

--- DoFit/aplication/protected/controllers/SiteController.php
+++ DoFit/aplication/protected/controllers/SiteController.php
@@ -29,7 +29,6 @@ class SiteController extends Controller
     {
         // renders the view file 'protected/views/site/index.php'
         // using the default layout 'protected/views/layouts/main.php'
-             $this->render('index');
         
         if (!isset(Yii::app()->session['id_usuario'])){
             if (isset(Yii::app()->session['admin'])) {
