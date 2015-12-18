43aa8c55476e16564675f8435584660539ebb63f
robertomontoto <monti.rober9@gmail.com>
2015-12-18 00:07:36 -0300

    Cambio de idioma Data table

--- DoFit/aplication/protected/views/profesorinstitucion/ListadoProfesores.php
+++ DoFit/aplication/protected/views/profesorinstitucion/ListadoProfesores.php
@@ -220,8 +220,33 @@ $this->pageTitle=Yii::app()->name;
 </div>
 <script type="text/javascript">
 $(document).ready(function() {
-    $('#lisprofesores').DataTable();
-} );
+    $('#lisprofesores').DataTable( {
+		"language" : {
+                      "sProcessing":     "Procesando...",
+	                  "sLengthMenu":     "Mostrar _MENU_ registros",
+	                  "sZeroRecords":    "No se encontraron resultados",
+	                  "sEmptyTable":     "Ningún dato disponible en esta tabla",
+	                  "sInfo":           "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
+	                  "sInfoEmpty":      "Mostrando registros del 0 al 0 de un total de 0 registros",
+	                  "sInfoFiltered":   "(filtrado de un total de _MAX_ registros)",
+	                  "sInfoPostFix":    "",
+	                  "sSearch":         "Buscar:",
+	                  "sUrl":            "",
+	                  "sInfoThousands":  ",",
+	                  "sLoadingRecords": "Cargando...",
+	                 "oPaginate": {
+		                           "sFirst":    "Primero",
+		                           "sLast":     "Ultimo",
+		                           "sNext":     "Siguiente",
+		                           "sPrevious": "Anterior"
+	                              },
+	                "oAria": {
+		                      "sSortAscending":  ": Activar para ordenar la columna de manera ascendente",
+		                      "sSortDescending": ": Activar para ordenar la columna de manera descendente"
+	                    } 
+		        }	
+    } );
+ } );
 </script>
 <script type="text/javascript">
     function Borrarprofesor(idprofesor)
@@ -230,7 +255,7 @@ $(document).ready(function() {
         var data = {"idprofesor":idprofesor};
         $.ajax({
             url :  baseurl + "/ProfesorInstitucion/BorrarProfesor",
-            type: "POST",
+            type: "POST", 
             dataType : "html",
             data : data,
             cache: false,
