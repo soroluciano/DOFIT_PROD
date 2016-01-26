953b7eccf0fce70e949fb7daeb46af4b475156eb
soroluciano <soro.luc@gmail.com>
2016-01-26 00:14:41 -0300

    paginacion casi terminada en imagenes

---
+++ DoFit/aplication/js/galeria.js
@@ -0,0 +1,40 @@
+
+    $(document).ready(function(){
+        debugger;
+         getPage(0);
+         getLinks(0);
+    });
+
+    function getLinks(page) {
+         $.ajax({
+          url: baseurl+'/galeria/getLinks',  
+          type: 'POST',
+          data: 'page='+page,
+          success:function(response){
+             debugger;
+             $('#menu-links').html(response);          
+          },
+          error: function(e){
+             alert(e);
+          }
+        });
+     
+    }
+    
+    function getPage(page){
+        $.ajax({
+          url: baseurl+'/galeria/getPage', 
+          type: 'POST',
+          data: 'page='+page,
+          success:function(response){
+             debugger;
+             $('#galeria-images').html(response);
+             getLinks(page);
+          },
+          error: function(e){
+             alert(e);
+          }
+        });
+    }
+    
+    
--- DoFit/aplication/protected/controllers/GaleriaController.php
+++ DoFit/aplication/protected/controllers/GaleriaController.php
@@ -28,8 +28,42 @@ class GaleriaController extends Controller
         $this->render('_imagenes');	
     }
     
-   
-    
-    
-  
-}
+	public function actionGetPage(){
+		$page = $_POST['page'];
+		$usuario = Usuario::model()->findByPk(Yii::app()->user->id);
+        $imagenes = Yii::app()->db->createCommand("select * from imagen where id_usuario = ".$usuario->id_usuario." limit ".$page.",8" )->queryAll(); 
+		 $mensaje="";
+          foreach($imagenes as $img){
+				$mensaje.= "<div class='col-md-3 col-sm-4 col-xs-6 img_class' ><img class='img-responsive' style='overflow:hidden;width=250px;height:250px' src='".Yii::app()->request->baseUrl."/uploads/".$img["nombre"]."' /></div>";
+          }
+		  echo $mensaje;
+		}
+		
+	public function actionGetLinks(){
+		$page = $_POST['page'];
+		$usuario = Usuario::model()->findByPk(Yii::app()->user->id);
+        $imagenes = Yii::app()->db->createCommand("select * from imagen where id_usuario = ".$usuario->id_usuario )->queryAll(); 
+        $count = count ($imagenes);
+		$max = 8;
+		$paginas = ceil ($count/$max);
+		$resultado = "";
+		$j = 0;
+		for($i=0; $i<$paginas; $i++){	
+		  $j = $i+1;
+		  if($page == $i){
+			$resultado.= "<li class='active'><a onclick='getPage(".$i.")'>".$j."</a></li>";
+		  	
+		  }else{
+				$resultado.= "<li class><a onclick='getPage(".$i.")'>".$j."</a></li>";
+		  
+		  }
+		  $j = 0;
+		
+		}
+		$res = "<ul class='pagination'>".$resultado."</ul>";
+		echo $res;
+		
+   }	
+		
+}	
+		
