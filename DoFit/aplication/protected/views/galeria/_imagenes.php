bd23d634461ed0c3ff29d4959ca8e94744ef052b
soroluciano <soro.luc@gmail.com>
2016-01-26 21:25:59 -0300

    mod

--- DoFit/aplication/protected/views/galeria/_imagenes.php
+++ DoFit/aplication/protected/views/galeria/_imagenes.php
@@ -3,17 +3,9 @@
 
 <div class="container_gallery">
 	<div class="row">
-    
 		<div id="galeria-images">
-				
 		</div>
 		<div id="menu-links">
-				
 		</div>
-   
-   
-   	
   </div>
-
-
 </div>
